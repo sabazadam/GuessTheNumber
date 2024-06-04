4e89534c91c87f6034e093058c45e8e73d12f3d0
sabazadam <78386440+sabazadam@users.noreply.github.com>
2024-06-04 13:59:04 +0300

    project finish! take a good care of the codes!

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
+from logo import logo
+from random import randint
+def gameStart():
+    global health
+    while health > 0:
+        guessNumber = int(input("Make a guess: "))
+        if guessNumber > randomNumber:
+            health -= 1
+            print("Too High!")
+            print("Guess Again.")
+            print(f"You have {health} attempts remaining to guess the number.")
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+        elif guessNumber < randomNumber:
+            health -= 1
+            print("Too low!")
+            print("Guess Again.")
+            print(f"You have {health} attempts remaining to guess the number.")
 
+        else:
+            print("Well done you win!")
+            return
+    print("YOU LOSE! Try again")
+print(logo)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Welcome to the game!")
+health = 0
+shouldContinue = True
+while shouldContinue:
+    randomNumber = randint(1, 100)
+    response = input("Please select level! Type 'easy' to play easy and type 'hard' to play hard. Also type 'q' to exit: ")
+    if response == "easy":
+        health = 10
+        gameStart()
+    elif response == "hard":
+        health = 5
+        gameStart()
+    elif response == "q":
+        shouldContinue = False
+        print("BYE!")
+    else:
+        print("invalid input!")
+        continue
